4b84de6645cd371e7fc42beea3a30cd0bb0e0108
Vinson Chen <viz@Vinsons-MacBook-Pro.local>
2017-02-02 01:10:19 -0800

    do rspec assignme nt

---
+++ rspec/spec/dessert_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require 'dessert'
+
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  subject(:dessert) {Dessert.new("sweet", 10, chef)}
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to be_truthy
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to be_truthy
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new("what", "is", chef)}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("sugar")
+      expect(dessert.ingredients).to include("sugar")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      stuff = %w(apple, mango, sugar, pie)
+      stuff.each { |stuff| dessert.add_ingredient(stuff) }
+      dessert.mix!
+      expect(dessert.ingredients).not_to eq(stuff)
+      expect(dessert.ingredients.sort).to eq(stuff.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      dessert.eat(5)
+      expect(dessert.quantity).to eq(5)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect{dessert.eat(1000)}.to raise_error("not enough left!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Jo the Baker")
+      expect(dessert.serve).to eq("Chef Jo the Baker has made 10 sweets!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(dessert)
+      dessert.make_more
+    end
+  end
+end
